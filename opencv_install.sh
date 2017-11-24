eebc4514147e056b5837510c1f1e840b736178a5
Kaiqiang Chen <chenkq@ipfserv4.ipf.kit.edu>
2017-11-24 12:02:52 +0100

    auto build dir

--- opencv_install.sh
+++ opencv_install.sh
@@ -10,6 +10,11 @@ echo $anaconda_include
 rm -rf build
 mkdir build
 cd build
+
+if [ ! -d "$install_prefix" ]; then
+    mkdir -p "$install_prefix"
+fi
+
 export LD_LIBRARY_PATH=$anaconda_lib:$LD_LIBRARY_PATH
 cmake ../ -DCMAKE_BUILD_TYPE=RELEASE -DWITH_CUDA=OFF -DBUILD_PYTHON_SUPPORT=ON -DPYTHON_INCLUDE_DIR=$anaconda_include/python2.7 -DPYTHON_LIBRARY=$anaconda_lib/libpython2.7.so -DCMAKE_INSTALL_PREFIX=$install_prefix/ -DPNG_LIBRARY_RELEASE=$anaconda_lib/libpng.so -DJPEG_LIBRARY_RELEASE=$anaconda_lib/libjpeg.so -DTIFF_LIBRARY_RELEASE=$anaconda_lib/libtiff.so
 
@@ -19,5 +24,3 @@ make install
 cp $install_prefix/lib/python2.7/site-packages/* $anaconda_lib/python2.7/site-packages/
 
 source ~/.bashrc
-
-cd
