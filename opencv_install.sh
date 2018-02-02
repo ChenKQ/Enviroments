81a01918a58cd05a8cfd586e03e1d7cc495bdecb
ChenKQ <ChenKQ@users.noreply.github.com>
2018-02-02 18:08:08 +0800

    Update opencv_install.sh
    add more python and numpy details support

--- opencv_install.sh
+++ opencv_install.sh
@@ -16,7 +16,7 @@ if [ ! -d "$install_prefix" ]; then
 fi
 
 export LD_LIBRARY_PATH=$anaconda_lib:$LD_LIBRARY_PATH
-cmake ../ -DCMAKE_BUILD_TYPE=RELEASE -DWITH_CUDA=OFF -DBUILD_PYTHON_SUPPORT=ON -DPYTHON_INCLUDE_DIR=$anaconda_include/python2.7 -DPYTHON_LIBRARY=$anaconda_lib/libpython2.7.so -DCMAKE_INSTALL_PREFIX=$install_prefix/ -DPNG_LIBRARY_RELEASE=$anaconda_lib/libpng.so -DJPEG_LIBRARY_RELEASE=$anaconda_lib/libjpeg.so -DTIFF_LIBRARY_RELEASE=$anaconda_lib/libtiff.so
+cmake ../ -DCMAKE_BUILD_TYPE=RELEASE -DWITH_CUDA=OFF -DBUILD_PYTHON_SUPPORT=ON -DPYTHON2_EXECUTABLE=$anaconda_root/bin/python2.7 -DPYTHON_INCLUDE_DIR=$anaconda_include/python2.7 -DPYTHON_LIBRARY=$anaconda_lib/libpython2.7.so -DPYTHON2_NUMPY_INCLUDE_DIRS=/home/chenkq/anaconda2/lib/python2.7/site-packages/numpy/core/include -DPYTHON2_PACKAGES_PATH=/home/chenkq/anaconda2/lib/python2.7/site-packages/numpy/core/include -DCMAKE_INSTALL_PREFIX=$install_prefix/ -DPNG_LIBRARY_RELEASE=$anaconda_lib/libpng.so -DJPEG_LIBRARY_RELEASE=$anaconda_lib/libjpeg.so -DTIFF_LIBRARY_RELEASE=$anaconda_lib/libtiff.so
 
 make -j8
 make install
