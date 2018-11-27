5d1f365d275ea82cade54c1507ac759531b9af44
quelle1 <iecas_wenkai@yahoo.com>
2018-11-27 12:10:17 +0800

    fix bug

--- opencv_install.sh
+++ opencv_install.sh
@@ -17,7 +17,7 @@ if [ ! -d "$install_prefix" ]; then
 fi
 
 export LD_LIBRARY_PATH=$anaconda_lib:$LD_LIBRARY_PATH
-cmake ../ -DCMAKE_BUILD_TYPE=RELEASE -DWITH_CUDA=OFF -DBUILD_PYTHON_SUPPORT=ON -DPYTHON_EXECUTABLE=$anaconda_root/bin/python$version -DPYTHON_INCLUDE_DIR=$anaconda_include/python$version -DPYTHON_LIBRARY=$anaconda_lib/python$version.so -DPYTHON_NUMPY_INCLUDE_DIRS=$anaconda_lib/python$version/site-packages/numpy/core/include -DPYTHON_PACKAGES_PATH=$anaconda_lib/python$version/site-packages/numpy/core/include -DCMAKE_INSTALL_PREFIX=$install_prefix/ -DPNG_LIBRARY_RELEASE=$anaconda_lib/libpng.so -DJPEG_LIBRARY_RELEASE=$anaconda_lib/libjpeg.so -DTIFF_LIBRARY_RELEASE=$anaconda_lib/libtiff.so
+cmake ../ -DCMAKE_BUILD_TYPE=RELEASE -DWITH_CUDA=OFF -DBUILD_PYTHON_SUPPORT=ON -DPYTHON3_EXECUTABLE=$anaconda_root/bin/python$version -DPYTHON_INCLUDE_DIR=$anaconda_include/python$version -DPYTHON_LIBRARY=$anaconda_lib/python$version.so -DPYTHON3_NUMPY_INCLUDE_DIRS=$anaconda_lib/python$version/site-packages/numpy/core/include -DPYTHON_PACKAGES_PATH=$anaconda_lib/python$version/site-packages -DCMAKE_INSTALL_PREFIX=$install_prefix/ -DPNG_LIBRARY_RELEASE=$anaconda_lib/libpng.so -DJPEG_LIBRARY_RELEASE=$anaconda_lib/libjpeg.so -DTIFF_LIBRARY_RELEASE=$anaconda_lib/libtiff.so
 
 make -j8
 make install
