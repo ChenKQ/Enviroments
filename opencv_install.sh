898be6ab334f67efa103eb4845132fccd1bcf362
root <root@localhost.localdomain>
2018-11-27 16:10:23 +0800

    extend

--- opencv_install.sh
+++ opencv_install.sh
@@ -3,7 +3,9 @@ homepath=`echo ~`
 anaconda_root=$homepath/anaconda3
 anaconda_lib=$anaconda_root/lib
 anaconda_include=$anaconda_root/include
+majorversion=3
 version=3.6
+subfix=3.6m
 install_prefix=$homepath/programs/opencv
 echo $anaconda_root
 echo $anaconda_lib
@@ -17,7 +19,7 @@ if [ ! -d "$install_prefix" ]; then
 fi
 
 export LD_LIBRARY_PATH=$anaconda_lib:$LD_LIBRARY_PATH
-cmake ../ -DCMAKE_BUILD_TYPE=RELEASE -DWITH_CUDA=OFF -DBUILD_PYTHON_SUPPORT=ON -DPYTHON3_EXECUTABLE=$anaconda_root/bin/python$version -DPYTHON_INCLUDE_DIR=$anaconda_include -DPYTHON_LIBRARY=$anaconda_lib/libpython${version}m.so -DPYTHON3_NUMPY_INCLUDE_DIRS=$anaconda_lib/python$version/site-packages/numpy/core/include -DPYTHON_PACKAGES_PATH=$anaconda_lib/python$version/site-packages -DCMAKE_INSTALL_PREFIX=$install_prefix/ -DPNG_LIBRARY_RELEASE=$anaconda_lib/libpng.so -DJPEG_LIBRARY_RELEASE=$anaconda_lib/libjpeg.so -DTIFF_LIBRARY_RELEASE=$anaconda_lib/libtiff.so
+cmake ../ -DCMAKE_BUILD_TYPE=RELEASE -DWITH_CUDA=OFF -DBUILD_PYTHON_SUPPORT=ON -DPYTHON$majorversion_EXECUTABLE=$anaconda_root/bin/python -DPYTHON_INCLUDE_DIR=$anaconda_include/python$subfix -DPYTHON_LIBRARY=$anaconda_lib/libpython$subfix.so -DPYTHON$majorversion_NUMPY_INCLUDE_DIRS=$anaconda_lib/python$version/site-packages/numpy/core/include -DPYTHON_PACKAGES_PATH=$anaconda_lib/python$version/site-packages -DCMAKE_INSTALL_PREFIX=$install_prefix/ -DPNG_LIBRARY_RELEASE=$anaconda_lib/libpng.so -DJPEG_LIBRARY_RELEASE=$anaconda_lib/libjpeg.so -DTIFF_LIBRARY_RELEASE=$anaconda_lib/libtiff.so
 
 make -j8
 make install
