a4c073815eeae28bde7b4b7cdfa54ddd3c4e6dab
chenkq <812545175@qq.com>
2018-12-07 21:01:16 +0800

    add cu90

--- docker/run_dlgpu.sh
+++ docker/run_dlgpu.sh
@@ -1,1 +1,1 @@
-nvidia-docker run -itd --name=dlgpu -p 8888:8888 -p 12222:22 -i -v /kaycharm/share:/var/mount dl:gpu /bin/bash
+nvidia-docker run -itd --name=dlgpu_nc -p 8888:8888 -p 12222:22 -i -v /kaycharm/share:/var/mount dl:gpu_nc /bin/bash
