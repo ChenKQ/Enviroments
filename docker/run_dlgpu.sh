318142e674d94bca88e23159e7f39436bf4da971
chenkq <812545175@qq.com>
2018-12-11 19:17:56 +0800

    fix jupyter IPython

--- docker/run_dlgpu.sh
+++ docker/run_dlgpu.sh
@@ -1,1 +1,1 @@
-nvidia-docker run -itd --name=dlgpu_nc -p 8888:8888 -p 12222:22 -i -v /kaycharm/share:/var/mount dl:gpu_nc /bin/bash
+nvidia-docker run -itd --name=dlgpu_nc -p 8888:8888 -p 12222:22 -i -v /kaycharm/share:/var/mount dl:gpu_cu90_nc /bin/bash
