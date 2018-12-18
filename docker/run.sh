dadf7fd21adc58a507125b5528ead509c02a389a
chenkq <chenkaiqiang14@mails.ucas.ac.cn>
2018-12-18 17:57:21 +0800

    add 6006 port

--- docker/run.sh
+++ docker/run.sh
@@ -1,1 +1,1 @@
-docker run -itd --name=test1 -p 8888:8888 -p 12222:22 -i -v /kaycharm/share:/var/mount dlcpu /bin/bash
+docker run -itd --name=test1 -p 8888:8888 -p 12222:22 -p 6006:6006 -i -v /kaycharm/share:/var/mount dlcpu /bin/bash
